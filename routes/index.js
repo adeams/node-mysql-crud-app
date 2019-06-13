7dac5a55de9dd1d4c1742b2064242caddeb78276
adeams <adeams@users.noreply.github.com>
2019-06-13 16:47:59 +0700

    test ok

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 module.exports = {
     getHomePage: (req, res) => {
-        let query = "SELECT * FROM `rowDataPLC` ORDER BY id ASC"; // query database to get all the players
+        //let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
+        let query = "SELECT * FROM `rawDataPLC` ORDER BY id ASC"; // query database to get all the players
 
         // execute query
         db.query(query, (err, result) => {
             if (err) {
                 res.redirect('/');
             }
+            console.log("result ->",result);
             res.render('index.ejs', {
                 title:" Welcome to Socka | View Players" 
                 ,players: result
